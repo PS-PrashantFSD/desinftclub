dc5c0400494f3f82759d1dbde1ef8a5d288c2f41
Psprasha369 <98733470+Psprasha369@users.noreply.github.com>
2022-02-11 17:12:51 +0530

    110220220511

--- wp-content/themes/dark-mode-for-a/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-define( 'darkmfa_version', '1.0' );
-add_action( 'wp_enqueue_scripts', 'darkmfa_enqueue_styles', 15 );
-function darkmfa_enqueue_styles() {
-wp_enqueue_style( 'darkmfa-css', get_stylesheet_directory_uri() . '/style.css', array( 'astra-theme-css' ), darkmfa_version, 'all' );
-}
